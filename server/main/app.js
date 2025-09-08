d295071b0c9e9bcbcdf210323b6fc3688cd86d4d
LacaoProtatooo <henrichkem.lacao@tup.edu.ph>
2025-09-08 19:08:54 +0800

    uhh..

---
+++ server/main/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+// Route Imports
+
+
+
+
+export default app; 
---
+++ server/main/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { connectDB } from "../config/db.js";
+import dotenv from "dotenv";
+import app from "./app.js";
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+console.log(process.env.MONGO_URI);
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log("Server Started at http://localhost:" + PORT);
+});
