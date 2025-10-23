aaeef9b7b43a69d61eab956e6bcac10bc0e8e293
chrollow <128967841+chrollow@users.noreply.github.com>
2025-10-23 19:45:52 +0800

    konting linis lang

--- server/config/db.js
+++ server/config/db.js
@@ -3,8 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI, { dbName: "TMOD" });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log(`Database Name: ${conn.connection.name}`); // Add this line
     } catch (error) {
         console.error(`Error: ${error.message}`)
         process.exit(1); // 1 means failure, 0 means success
